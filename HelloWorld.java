17e031560f778e70db053cb570bb96cd04496683
Nikhil Bhalkar <115405669+nikhilbhalkar@users.noreply.github.com>
2025-09-06 12:46:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins! ....8");
+        System.out.println("Hello, Jenkins! ....8")
     }
 }
